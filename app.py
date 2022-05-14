4c732272482caa622f40b7f9bdbf79d11d7f8c22
Daniel Bortolato <daniel.bortolato@gmail.com>
2022-05-14 10:21:36 -0300

    start-menu-prompt-tests

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+MENU_PROMPT = 'Enter a option: "C" to create a blog, "L" to list blogs, "R" to read one, "P" to create a post or "Q" to quit.'
 blogs = dict()
 
 def menu():
 
     print_blogs()
+    selection = input(MENU_PROMPT)
 
 def print_blogs():
     for key, blog in blogs.items():
