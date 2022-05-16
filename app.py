63871387a9308004ec7382e726c722e0eee1b6d3
Daniel Bortolato <daniel.bortolato@gmail.com>
2022-05-16 16:43:01 -0300

    menu_addons

--- app.py
+++ app.py
@@ -5,7 +5,25 @@ def menu():
 
     print_blogs()
     selection = input(MENU_PROMPT)
-
+    while selection != 'Q':
+        if selection == 'C':
+            ask_create_blog()
+        elif selection == 'L':
+            print_blogs()
+        elif selection == 'R':
+            ask_read_blogs()
+        elif selection == 'P':
+            ask_create_post()
+        selection = input(MENU_PROMPT)
 def print_blogs():
     for key, blog in blogs.items():
         print('- {}'.format(blog))
+
+def ask_create_blog():
+    pass
+
+def ask_read_blogs():
+    pass
+
+def ask_create_post():
+    pass
